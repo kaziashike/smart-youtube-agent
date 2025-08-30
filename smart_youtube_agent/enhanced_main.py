d23c103414af8afbbc8dec2219c5d60a57e9b66c
kaziashike <kaziashike@github.com>
2025-08-31 02:54:03 +0600

    Fix import using relative path

--- smart_youtube_agent/enhanced_main.py
+++ smart_youtube_agent/enhanced_main.py
@@ -13,7 +13,7 @@ from typing import Dict, Any, Optional
 import asyncio
 
 # Import all modules
-from auth import get_current_user
+from .auth import get_current_user
 from enhanced_auth_routes import router as auth_router
 from saas_routes import router as saas_router
 from dashboard import router as dashboard_router
